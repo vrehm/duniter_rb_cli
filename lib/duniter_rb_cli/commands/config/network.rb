0055b90e7ede9520f34f053711e400813baaa919
Vincent REHM <vincentrehm@yahoo.fr>
2019-01-06 17:02:12 +0100

    adding base_url for each network

--- lib/duniter_rb_cli/commands/config/network.rb
+++ lib/duniter_rb_cli/commands/config/network.rb
@@ -14,16 +14,20 @@ module DuniterRbCli
           @config.extname = '.yml'
           @config.append_path Dir.pwd
           @config.append_path Dir.home
+          @config.read if @config.exist?
+          @actual_config = @config.fetch(:name)
         end
 
         def execute(input: $stdin, output: $stdout)
-          @config.read if @config.exist?
-          actual_config = @config.fetch(:name)
-          output.puts "Config value: #{actual_config}" unless actual_config.nil?
+          output.puts "Config value: #{@actual_config}" unless @actual_config.nil?
           user_response = prompt.select("Set G1 or G1-test:", %w(G1-test G1))
           @config.set :name, value: user_response
+          user_response == "G1" ?
+            @config.set(:base_url, value: "https://g1.duniter.org/") :
+            @config.set(:base_url, value: "https://g1-test.duniter.org/")
           @config.write(force: true)
           output.puts "Value successfully set to: #{@config.fetch(:name)}"
+          output.puts "Base url: #{@config.fetch(:base_url)}"
         end
       end
     end
